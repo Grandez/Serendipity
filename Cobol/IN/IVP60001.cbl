dd42481f7d5de37eb3af93c744aed4c1927640bc
Grandez <javier.gonzalez.grandez@gmail.com>
2018-01-13 20:27:17 +0100

    As 17/01/13

---
+++ Cobol/IN/IVP60001.cbl
@@ -0,0 +1,33 @@
+      *> SDP DESC CICS RESP                                                     
+      *> SDP IVP 0 Issues.getCount = 1 - NOHANDLE no permitido                       
+       IDENTIFICATION DIVISION.                                                 
+       PROGRAM-ID. AIVP60001                                                    
+       AUTHOR.     USRIVP0 - SERENDIPITY                                     
+       DATE-COMPILED. 01/01/2015.                                               
+       
+       ENVIRONMENT DIVISION.                                                    
+       CONFIGURATION SECTION.                                                   
+       
+       DATA DIVISION.                                                           
+       WORKING-STORAGE SECTION.                                                 
+                                                                                
+       01 W-FECHAS-TRABAJO.                                                     
+          05 WKT-ABSTIME          PIC S9(15) COMP-3.                            
+          05 WKT-AAAAMMDD-HOY.                                                  
+            10 WKT-ANIO-HOY.                                                    
+              15 WKT-SS-HOY      PIC 9(02) VALUE 20.                            
+              15 WKT-AA-HOY      PIC 9(02).                                     
+            10 WKT-AAAA-HOY     REDEFINES WKT-ANIO-HOY PIC 9(04).               
+            10 WKT-MM-HOY       PIC 9(02).                                      
+            10 WKT-DD-HOY       PIC 9(02).                                      
+                                                                                
+       PROCEDURE DIVISION.                                                      
+       000000-CONTROL.                                                          
+            EXEC CICS                                                           
+                 ASKTIME                                                        
+                 ABSTIME (WKT-ABSTIME)                                          
+                 NOHANDLE                                                       
+            END-EXEC                                                            
+            EXEC CICS                                                           
+                 RETURN                                                         
+            END-EXEC.                                                           
